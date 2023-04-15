a77cd930d7187dd3b7c6e067f1b633dec4eb74f2
aidyninho <ryzyk.aidyn@gmail.com>
2023-04-16 05:40:07 +0600

    made deleteUser public.

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -114,7 +114,7 @@ public class SimpleJDBCRepository {
         }
     }
 
-    private void deleteUser(Long userId) {
+    public void deleteUser(Long userId) {
         try {
             connection = CustomDataSource.getInstance().getConnection();
             ps = connection.prepareStatement(deleteUser);
